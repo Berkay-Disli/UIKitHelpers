2da778bb5248e91e528a1c092705c1f9d9340097
Berkay Dişli <berkay.dsli@gmail.com>
2023-11-29 23:10:04 +0300

    Create MovingNavBar.swift

---
+++ MovingNavBar.swift
@@ -0,0 +1,8 @@
+// Use in extension with scroll view delegate
+
+func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
